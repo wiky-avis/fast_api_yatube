f3e613edce0bc78a80ed86c34e0ae78dfba99b51
wiky-avis <peta-holding@yandex.ru>
2021-05-25 17:23:32 +0300

    update  models

--- fast_api_yatube/posts/models.py
+++ fast_api_yatube/posts/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class Group(models.Model):
-    title = models.CharField(max_length=200, verbose_name='Название')
+    title = models.CharField(max_length=200, verbose_name='Название группы')
 
     def __str__(self):
         return self.title
@@ -14,38 +14,49 @@ class Group(models.Model):
 
 
 class Post(models.Model):
-    text = models.TextField()
+    text = models.TextField('Текст поста')
     pub_date = models.DateTimeField(
-        'Дата публикации', auto_now_add=True
-    )
+        'Дата публикации', auto_now_add=True)
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='posts'
-    )
+        User,
+        on_delete=models.CASCADE,
+        related_name='posts',
+        verbose_name='Автор поста')
     image = models.ImageField(
-        upload_to='posts/', null=True, blank=True
-    )
+        'Изображение', upload_to='posts/', null=True, blank=True)
     group = models.ForeignKey(
         Group,
         on_delete=models.SET_NULL,
         related_name='posts',
         null=True,
-        blank=True)
+        blank=True, verbose_name='Группа')
 
     def __str__(self):
         return self.text
+
+    class Meta:
+        verbose_name_plural = 'Посты'
+        verbose_name = 'Пост'
 
 
 class Comment(models.Model):
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='comments'
-    )
+        User,
+        on_delete=models.CASCADE,
+        related_name='comments',
+        verbose_name='Автор комментария')
     post = models.ForeignKey(
-        Post, on_delete=models.CASCADE, related_name='comments'
-    )
+        Post,
+        on_delete=models.CASCADE,
+        related_name='comments',
+        verbose_name='Пост')
     text = models.TextField('Комментарий', default='')
     created = models.DateTimeField(
-        'Дата добавления', auto_now_add=True, db_index=True
-    )
+        'Дата добавления', auto_now_add=True, db_index=True)
 
     def __str__(self):
         return self.text
+
+    class Meta:
+        verbose_name_plural = 'Комментарии'
+        verbose_name = 'Комментарий'
